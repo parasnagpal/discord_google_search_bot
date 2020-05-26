6c648fcc06428893eb3ede49a40cd31a3c36b3e9
Paras Nagpal <paras.nag@gmail.com>
2020-05-26 16:34:57 +0530

    Semicolons and comments

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ const axios= require('axios');
 
 //Login handle
 client.on('ready',()=>{
+    //Log a greeting message to console.
     console.log("Hello from "+client.user.tag);
 })
 
 client.on('message',msg=>{
+    //identify if a search request is made
     if(msg.content.startsWith('!google')){
+        //extract the required search string
         let str=msg.content.slice(7).trim();
+        //Axios request to Google Custom Search API
         axios.get(`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_CUSTOM_SEARCH_API_KEY}&cx=${process.env.GOOGLE_SEARCH_ENGINE_ID}&q=${str}`)
         .then((search)=>{
+            //create a new object of MessageEmbed and set properties
             const cse_embed= new Discord.MessageEmbed()
             .setColor('#000fff')
 	        .setTitle("Search results for:"+str)
@@ -20,26 +25,27 @@ client.on('message',msg=>{
 	        .addFields(
                 ...return_search_result(search.data.items)
 	        )
-            
             msg.channel.send(cse_embed)
         })
         .catch(e=>{
-            console.log(e)
-            msg.reply("Search Error")
+            //log any error during Google CSE request
+            console.log(e);
+            //send and error message
+            msg.reply("Search Error");
         })
     }
 })
 
 function return_search_result(search_data){
     let cropped_data=search_data.slice(0,5);
-    let display=[]
+    let display=[];
     cropped_data.map((result)=>{
         display.push({
             name:result.title,
             value:result.snippet+` [Click here](${result.link})`,
         }) 
-    })
-    return display              
+    });
+    return display;              
 }
 
 client.login(process.env.DISCORD_TOKEN)
